cdca4b38081b873bec67cf2ad5f19027d3bd3155
mosida <ayshot0918@gmail.com>
2021-06-14 21:19:08 +0900

    スコア計算処理追加

---
+++ TetrisLogic/GameManager/ScoreManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisLogic
+{
+    public class ScoreManager
+    {
+        public int Score { get; private set; }
+
+
+        public ScoreManager()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Score = 0;
+        }
+
+
+        public void Add(int line, TSpinTypes tSpin, int ren, bool btb, bool allClear)
+        {
+            var ratio = 0;
+            if(tSpin == TSpinTypes.tSpin)
+            {
+                ratio += line == 1 && tSpin == TSpinTypes.tSpin ? 3 :
+                line == 2 && tSpin == TSpinTypes.tSpin ? 5 :
+                line == 3 && tSpin == TSpinTypes.tSpin ? 7 : 0;
+            }
+            else
+            {
+                ratio += line == 1 ? 1 :
+                    line == 2 ? 2 :
+                    line == 3 ? 3 :
+                    line == 4 ? 5 : 0;
+            }
+
+            ratio += ren switch
+                {
+                    0 => 0,
+                    1 => 0,
+                    2 => 1,
+                    3 => 1,
+                    4 => 2,
+                    5 => 2,
+                    6 => 3,
+                    7 => 3,
+                    8 => 4,
+                    9 => 4,
+                    10 => 4,
+                    _ => 5,
+                };
+
+            ratio += btb ? 1 : 0;
+
+            ratio += allClear ? 10 : 0;
+
+            var score = ratio * 100;
+            Score += score;
+
+        }
+    }
+}
