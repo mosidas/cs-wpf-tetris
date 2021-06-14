74edf2322cbe6cc58fb8dd1cc769fb396b7c27f5
mosida <ayshot0918@gmail.com>
2021-06-14 23:38:30 +0900

    null安全

---
+++ TetrisLogic/UserAction/UA_Nothing.cs
@@ -0,0 +1,15 @@
+﻿namespace TetrisLogic.UserAction
+{
+    class UA_Nothing : IUserAction
+    {
+        public void Action(ref Field field, ref Block currentBlock, ref Block holdBlock)
+        {
+            
+        }
+
+        public bool CanAction(Field field, Block block, Block holdBlock)
+        {
+            return true;
+        }
+    }
+}
