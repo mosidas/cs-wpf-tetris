2879c99f8932d9578cf48c1aa39961b929ea15e8
mosida <ayshot0918@gmail.com>
2021-06-15 20:05:31 +0900

    追加

---
+++ TetrisWindow/MessageWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TetrisWindow
+{
+    /// <summary>
+    /// MessageWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MessageWindow : UserControl
+    {
+        public MessageWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
